3d3e53c28d913a9c7a89ce2d64746e557797efb9
AnjaliRSahu <anjalisahu1996@gmail.com>
2022-09-13 21:20:22 +0530

    [Anjali]AddedMainFile

---
+++ LinkedListPracticeProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListPracticeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linked List Program");
+            Console.ReadLine();
+        }
+    }
+}
